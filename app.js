78ea355e70402913757979ac1fae340fd4158340
Skaind <skaind41@gmail.com>
2015-06-18 13:50:25 +0200

    CSS adaptable a moviles

--- app.js
+++ app.js
@@ -15,9 +15,10 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(partials());
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(partials());
 app.use(favicon(__dirname + '/public/images/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
