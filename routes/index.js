fe3eb659570e5b890118bc2fc8bf4297f033e6e8
Skaind <skaind41@gmail.com>
2015-06-19 09:42:02 +0200

    Pagina Creditos

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,8 @@ router.get('/', function(req, res) {
 router.get( '/quizes/question', quizController.question);
 router.get( '/quizes/answer', quizController.answer);
 
+router.get('/author', function(req, res) {
+  res.render('author', { creditos: 'Proyecto creado por Dani Ramos' });
+});
+
 module.exports = router;
